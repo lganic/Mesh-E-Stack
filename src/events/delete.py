6c7fb3c294e7ee1c032080eb80f6b6032af22c6e
Logan Boehm <sidwirb@gmail.com>
2025-09-10 16:33:14 -0400

    refactored delete event to use set

--- src/events/delete.py
+++ src/events/delete.py
@@ -1,12 +1,18 @@
-from typing import List
+from typing import Set
 
 from .event import Event
 from ..objects import Vertex, Triangle
 
 class DeleteEvent(Event):
 
-    def __init__(self, verts: List[Vertex] = [], tris: List[Triangle] = []):
+    def __init__(self, verts: Set[Vertex] = None, tris: Set[Triangle] = None):
         super().__init__()
+
+        if verts is None:
+            verts = set()
+        
+        if tris is None:
+            tris = set()
         
         self._verts = verts
         self._tris = tris
