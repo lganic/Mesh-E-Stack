2591eb8b0fc52edfc1ff708aa78f82d6ddb23930
Logan Boehm <sidwirb@gmail.com>
2025-09-10 16:36:25 -0400

    reworked stack to use double ended queue instead of list

--- src/stack/stack.py
+++ src/stack/stack.py
@@ -1,10 +1,12 @@
+from collections import deque
+
 from ..objects import Mesh
 
 class Stack:
 
     def __init__(self, mesh_reference: Mesh = None):
 
-        self.events = []
+        self.events = deque()
 
         if mesh_reference is None:
             mesh_reference = Mesh()
