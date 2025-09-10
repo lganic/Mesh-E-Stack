9a225e7a225c517cb2157965525a4b4fa4f01323
Logan Boehm <sidwirb@gmail.com>
2025-09-10 16:13:37 -0400

    Added data to delete event

--- src/events/delete.py
+++ src/events/delete.py
@@ -1,7 +1,12 @@
+from typing import List
+
 from .event import Event
+from ..objects import Vertex, Triangle
 
 class DeleteEvent(Event):
 
-    def __init__(self):
+    def __init__(self, verts: List[Vertex] = [], tris: List[Triangle] = []):
         super().__init__()
         
+        self._verts = verts
+        self._tris = tris
