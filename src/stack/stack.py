fab4744f8efad73976365afa187d1788d07f412f
Logan Boehm <sidwirb@gmail.com>
2025-09-10 16:43:42 -0400

    added delete event to stack

--- src/stack/stack.py
+++ src/stack/stack.py
@@ -1,6 +1,8 @@
 from collections import deque
+from typing import Set
 
-from ..objects import Mesh
+from ..objects import Mesh, Vertex, Triangle
+from .. import Event
 
 class Stack:
 
@@ -12,3 +14,10 @@ class Stack:
             mesh_reference = Mesh()
 
         self.mesh_reference = mesh_reference
+
+    def _add_event(self, event: Event):
+        self.events.append(event)
+
+    def delete(self, verts_to_remove: Set[Vertex] = None, tris_to_remove: Set[Triangle] = None, remove_orphaned_verts = True):
+
+        self._add_event(self.mesh_reference.delete_objects(verts_to_remove = verts_to_remove, tris_to_remove = tris_to_remove, remove_orphaned_verts = remove_orphaned_verts))
