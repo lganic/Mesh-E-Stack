73c98ea9fe1a44e79dc96bce610e226760037364
Logan Boehm <sidwirb@gmail.com>
2025-09-10 15:59:00 -0400

    Added initial classes for add and modify events

---
+++ src/events/add.py
@@ -0,0 +1,6 @@
+from event import Event
+
+class AddEvent(Event):
+    
+    def __init__(self):
+        super().__init__()
---
+++ src/events/modify.py
@@ -0,0 +1,7 @@
+from .event import Event
+
+class ModifyEvent(Event):
+
+    def __init__(self):
+        super().__init__()
+        
