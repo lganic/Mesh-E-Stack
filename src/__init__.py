991b33b1f0abf2f83766b3a7e3dac697721e7a92
Logan Boehm <sidwirb@gmail.com>
2025-09-10 16:07:33 -0400

    Added initial implementation of stack object

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 from .objects import Mesh, Triangle, Vertex
+from .stack import Stack
---
+++ src/stack/stack.py
@@ -0,0 +1,12 @@
+from ..objects import Mesh
+
+class Stack:
+
+    def __init__(self, mesh_reference: Mesh = None):
+
+        self.events = []
+
+        if mesh_reference is None:
+            mesh_reference = Mesh()
+
+        self.mesh_reference = mesh_reference
