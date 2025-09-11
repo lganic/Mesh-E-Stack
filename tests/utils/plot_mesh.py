a3358439964b02591aba3f1d647a40281657b770
Logan Boehm <sidwirb@gmail.com>
2025-09-11 14:05:11 -0400

    updated mesh plotter to also shade resulting triangles.

--- tests/utils/plot_mesh.py
+++ tests/utils/plot_mesh.py
@@ -5,6 +5,7 @@ if TYPE_CHECKING:
 
 from .generate_heart import generate_heart
 import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
 import numpy as np
 
 def plot_raw_mesh(points, triangles, show = True):
@@ -15,15 +16,19 @@ def plot_raw_mesh(points, triangles, show = True):
     - points: list or numpy array of shape (N, 2), coordinates of the vertices.
     - triangles: list of triplets (i, j, k) with indices into `points`.
     """
+
     points = np.array(points)
+    # Build list of triangle vertex coordinates
+    polys = [points[list(tri)] for tri in triangles]
 
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
 
-    for tri in triangles:
-        tri_points = points[list(tri) + [tri[0]]]  # close the triangle
-        ax.plot(tri_points[:, 0], tri_points[:, 1], 'k-')
+    # Add shaded triangles
+    coll = PolyCollection(polys, facecolors="lightblue", edgecolors="k", linewidths=1)
+    ax.add_collection(coll)
 
+    # Plot vertices
     ax.scatter(points[:, 0], points[:, 1], c='red', zorder=5)
 
     if show:
