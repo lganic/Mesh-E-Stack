646c6ddee94783d922a1404e39b198c810b7dfcb
Logan Boehm <sidwirb@gmail.com>
2025-09-11 14:05:31 -0400

    Added heart plot test

--- tests/plots/test_plot.py
+++ tests/plots/test_plot.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 import pytest
 
+from ..utils import generate_heart, plot_mesh
+
 @pytest.mark.mpl_image_compare
 def test_plot():
     fig, ax = plt.subplots()
     ax.plot([1, 3, 300])
+    return fig
+
+@pytest.mark.mpl_image_compare
+def test_heart_plot():
+    fig = plot_mesh(generate_heart(), show = False)
+
     return fig
