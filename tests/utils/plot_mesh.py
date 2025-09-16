8404939e76bff15e6e1cbf12323afb1bc64fe238
Logan Boehm <sidwirb@gmail.com>
2025-09-16 14:07:03 -0400

    Testing out point winding order restriction to enforce test cases in non-deterministic scenarios.

--- tests/utils/plot_mesh.py
+++ tests/utils/plot_mesh.py
@@ -8,6 +8,42 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import PolyCollection
 import numpy as np
 
+def winding_order(p1, p2, p3):
+    """
+    Determine winding order of three 2D points.
+
+    Returns:
+        "CCW" if counter-clockwise,
+        "CW" if clockwise,
+        "COLINEAR" if the points are colinear
+    """
+    # Compute signed area (2D cross product of vectors p1->p2 and p1->p3)
+    area = (p2[0] - p1[0]) * (p3[1] - p1[1]) - \
+           (p2[1] - p1[1]) * (p3[0] - p1[0])
+
+    if area > 0:
+        return "CCW"
+    elif area < 0:
+        return "CW"
+    else:
+        return "COLINEAR"
+
+
+def force_cw_order(points):
+
+    order = winding_order(points[0], points[1], points[2])
+
+    if order == "COLINEAR":
+        return points # Throw error maybe? 
+
+    if order == "CCW":
+        return points[::-1]
+    
+    return points
+
+def force_cw_point_group(polys):
+    return [force_cw_order(group) for group in polys]
+
 def plot_raw_mesh(points, triangles, show = True):
     """
     Plot a 2D triangular mesh from points and triangle indices.
@@ -20,6 +56,8 @@ def plot_raw_mesh(points, triangles, show = True):
     points = np.array(points)
     # Build list of triangle vertex coordinates
     polys = [points[list(tri)] for tri in triangles]
+
+    polys = force_cw_point_group(polys)
 
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
